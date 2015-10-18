33ae75925158e58117ca76ff87a5a4706b33ae9f
Labedlam <Zeo@Zeeshans-MacBook-Pro.local>
2015-10-18 12:48:46 -0500

    Initial commit

---
+++ prime_prestudy_bonus_week_03.js
@@ -0,0 +1,19 @@
+function nameA(number){
+	var answerA;
+	answerA= number +5;
+	return answerA;
+};
+
+function nameB(number){
+var answerB;
+	answerB= number +10;
+	return answerB;
+};
+function nameC(number){
+var answerC;
+	answerC= number +2;
+	return answerC;
+};
+
+console.log(nameA(nameB(nameC(2))));
+
